ae7cd45821dbd5d749ab871a0a3724f3a2ac2701
juyoen <remake111@medistaff.co.kr>
2023-02-11 13:43:40 +0900

    구글 지도 띄우기

--- lib/map.dart
+++ lib/map.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'search.dart';
 
-class Map extends StatelessWidget {
+class Map extends StatefulWidget {
   const Map({super.key});
 
   @override
+  State<Map> createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
+              myLocationButtonEnabled: false,
+              myLocationEnabled: true,
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+            ),
+            Positioned(
+                top: 50,
+                left: 8,
+                right: 8,
+                child: Container(
+                    color: Colors.white,
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Search()));
+                        },
+                        child: Row(
+                          children: [
+                            Icon(Icons.search),
+                            Text('장소, 주소  검색'),
+                          ],
+                        ))))
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Navigator.push(context,
+            //     MaterialPageRoute(builder: (context) => const CreatePost()));
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
